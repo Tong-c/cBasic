1287509d3cc01ececb1670c5181a02977205371c
Tong-c <1972376180@qq.com>
2018-06-13 09:37:49 +0800

    1.comment 2.constant

---
+++ chapterTwo/comment.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+* comment1
+* main:generate some simple output
+*/
+
+int main(void)
+{	printf(/* comment2 */ "Hello World.\n");/* comment3 */
+	return 0;
+}
---
+++ chapterTwo/tconstant.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("character: %c\ninteger: %d\nfloating point: %f\n",')',34,3.14);
+}
+
